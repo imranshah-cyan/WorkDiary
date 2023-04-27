c300b00a11e040cffd9b157ba2e5f3e21aa7fe46
AdilShahab <adilislamian92@gmail.com>
2023-04-27 16:37:41 +0500

    WebLogs API
    API for Work Diary Web Version hourly logs for both the manager and the provider.

---
+++ WorkDiary.Repositories/Interfaces/IWebLogsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkDiaryRepository.Dbo;
+
+namespace WorkDiaryRepository.Interfaces
+{
+    public interface IWebLogsRepository
+    {
+        List<Web_GetWorkSummaryImagesByProviderAndJob_Result> GetWebLogs(DateTime start_date, DateTime end_date, int provider_id, int job_id);
+    }
+}
